9d08c249b73c688409cead2b89be43f1db6ba5a7
AlejandroVeras <verasetanislao21@gmail.com>
2024-06-14 17:55:36 -0700

    Create acceder_y_modificar_elementos.py

---
+++ acceder_y_modificar_elementos.py
@@ -0,0 +1,10 @@
+# Creamos una lista
+frutas = ['manzana', 'pera', 'naranja', 'plátano']
+
+# Accedemos al tercer elemento de la lista
+print(frutas[2])  # naranja
+
+# Modificamos el tercer elemento de la lista
+frutas[2] = 'limón'
+
+print(frutas)  # ['manzana', 'pera', 'limón', 'plátano']
