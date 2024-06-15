1cafe187462b4b69993ff02b132e084faadce6ce
AlejandroVeras <verasetanislao21@gmail.com>
2024-06-14 17:54:39 -0700

    Create operaciones_matemáticas.py

---
+++ operaciones_matemáticas.py
@@ -0,0 +1,10 @@
+# Creamos una lista de números
+numeros = [1, 2, 3, 4, 5]
+
+# Calculamos la suma de los elementos de la lista
+suma = sum(numeros)
+
+# Calculamos la media dividiendo la suma entre la cantidad de elementos
+media = suma / len(numeros)
+
+print(media)  # 3.0
